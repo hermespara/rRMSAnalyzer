2fa4c2008c58d4f2cafe769e205bda17032e7bcf
hermespara <paraqindes.hermes@gmail.com>
2026-04-07 14:14:53 +0200

    Add sites parameter to the heatmap

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -6,11 +6,18 @@
 #' named character vector when \code{color_col} has length 1, or a named list
 #' of named character vectors when multiple metadata columns are used.
 #' @param only_annotated Use only annotated sites (default = TRUE).
+#' @param sites A character vector of specific annotated site names to use for
+#' the heatmap (e.g., \code{c("28S_Am1310", "18S_Am99")}). Cannot be used
+#' together with \code{only_annotated = TRUE}.
 #' @param title Title to display on the plot. "default" for default title.
 #' @param cutree_rows number of clusters the rows are divided into, based on the hierarchical clustering (using cutree).
 #' @param cutree_cols number of clusters the columns are divided into, based on the hierarchical clustering (using cutree).
+#' @param clustering_distance Distance metric used for hierarchical clustering
+#' of both rows and columns. Default is \code{"manhattan"}.
+#' @param clustering_method Linkage method used for hierarchical clustering of
+#' both rows and columns. Default is \code{"ward.D2"}.
 #' @param ... Pheatmap’s parameters
-#' @return A ggplot object of a heatmap. The distance used is manhattan and the clustering method is Ward.D2. See ComplexHeatmap doc for more details
+#' @return A ggplot object of a heatmap. See ComplexHeatmap doc for more details
 #' @export
 #'
 #' @examples
@@ -20,23 +27,32 @@
 #' # ribo_toy <- annotate_site(ribo_toy,human_methylated)
 #' # plot_heatmap(ribo_toy,  color_col = c("run","condition"), only_annotated=TRUE)
 plot_heatmap <- function(ribo, color_col = NULL, only_annotated = FALSE, title,
-                         cutree_rows = 4, cutree_cols = 2,
+                         sites = NULL, cutree_rows = 4, cutree_cols = 2,
+                         clustering_distance = "manhattan",
+                         clustering_method = "ward.D2",
                          sample_colors = NULL, ...) {
   check_is_se(ribo)
   check_type(only_annotated, "logical", "only_annotated", length = 1)
+  check_type(sites, "character", "sites")
   check_type(cutree_rows, "numeric", "cutree_rows", length = 1)
   check_type(cutree_cols, "numeric", "cutree_cols", length = 1)
+  check_type(clustering_distance, "character", "clustering_distance", length = 1)
+  check_type(clustering_method, "character", "clustering_method", length = 1)
 
   check_metadata(ribo, color_col)
   matrix <- extract_data(ribo, "cscore",
     position_to_rownames = TRUE,
-    only_annotated = only_annotated
+    only_annotated = only_annotated,
+    sites = sites
   )
 
   .plot_heatmap(matrix, SummarizedExperiment::colData(ribo),
     color_col = color_col,
     most_variant = FALSE, title = title, cutree_rows = cutree_rows,
-    cutree_cols = cutree_cols, sample_colors = sample_colors, ...
+    cutree_cols = cutree_cols,
+    clustering_distance = clustering_distance,
+    clustering_method = clustering_method,
+    sample_colors = sample_colors, ...
   )
 }
 
@@ -53,7 +69,9 @@ plot_heatmap <- function(ribo, color_col = NULL, only_annotated = FALSE, title,
 .plot_heatmap <- function(cscore_matrix = NULL, metadata = NULL,
                           color_col = NULL, most_variant = FALSE,
                           title = "default", cutree_rows,
-                          cutree_cols, sample_colors = NULL, ...) {
+                          cutree_cols, clustering_distance = "manhattan",
+                          clustering_method = "ward.D2",
+                          sample_colors = NULL, ...) {
   heat_colors <- grDevices::hcl.colors(7, "inferno")
 
 
@@ -71,10 +89,10 @@ plot_heatmap <- function(ribo, color_col = NULL, only_annotated = FALSE, title,
     row_title = "Position", column_title = "Sample",
     column_title_side = "bottom",
     cluster_rows = TRUE, cluster_columns = TRUE,
-    clustering_distance_columns = "manhattan",
-    clustering_distance_rows = "manhattan",
-    clustering_method_columns = "ward.D2",
-    clustering_method_rows = "ward.D2",
+    clustering_distance_columns = clustering_distance,
+    clustering_distance_rows = clustering_distance,
+    clustering_method_columns = clustering_method,
+    clustering_method_rows = clustering_method,
     row_split = cutree_rows, column_split = cutree_cols,
     top_annotation = column_ha,
     row_names_gp = grid::gpar(fontsize = 6)
